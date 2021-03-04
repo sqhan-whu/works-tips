1a73339a1d2a615846d7c1ccb4fd63f2acdc3280
Han Shaoqing <502466541@qq.com>
2021-03-04 21:51:36 +0800

    Update ANNOVAR.hg38.sh

--- ANNOVAR.hg38.sh
+++ ANNOVAR.hg38.sh
@@ -8,24 +8,23 @@ solve 3 problem:
 
 #perl annotate_variation.pl -buildver mm10 -downdb -webfrom annovar refGene GRmm38db/
 ### change refGene to ensGene
-# wget http://hgdownload.cse.ucsc.edu/goldenPath/mm10/database/wgEncodeGencodeBasicVM24.txt.gz
-# gunzip wgEncodeGencodeBasicVM24.txt.gz 
-#mv wgEncodeGencodeBasicVM24.txt mm10_ensGene.txt
+wget http://hgdownload.cse.ucsc.edu/goldenPath/hg38/database/wgEncodeGencodeBasicV31.txt.gz
+# gunzip wgEncodeGencodeBasicV31.txt.gz
+#mv wgEncodeGencodeBasicV31.txt hg38_ensGene.txt
+perl annotate_variation.pl --buildver hg38 --downdb seq hg38db/hg38_seq
 
-perl annotate_variation.pl --buildver mm10 --downdb seq GRmm38db/mm10_seq
+#perl retrieve_seq_from_fasta.pl hg38db/mm10_refGene.txt -seqdir GRmm38db/mm10_seq -format refGene -outfile GRmm38db/mm10_refGeneMrna.fa
 
-#perl retrieve_seq_from_fasta.pl GRmm38db/mm10_refGene.txt -seqdir GRmm38db/mm10_seq -format refGene -outfile GRmm38db/mm10_refGeneMrna.fa
-
-perl retrieve_seq_from_fasta.pl GRmm38db/mm10_ensGene.txt -seqdir GRmm38db/mm10_seq -format ensGene -outfile GRmm38db/mm10_refGeneMrna.fa
+perl retrieve_seq_from_fasta.pl hg38db/hg38_ensGene.txt -seqdir hg38db/hg38_seq -format ensGene -outfile hg38db/hg38_refGeneMrna.fa
 ### same as above 
 #perl retrieve_seq_from_fasta.pl -format genericGene -seqfile GRmm38db/mm10_seq/mm10.fa -outfile GRmm38db/mm10_wgEncodeGencodeBasicVM24Mrna.fa GRmm38db/mm10_ensGene.txt
 
-### download mm10 snp database Version snp142;  2 type snp,  total snp / Common snp  ~  10 / 1,     Common recommend
-#perl annotate_variation.pl -downdb -buildver mm10 -webfrom ucsc snp142 GRmm38db
-perl annotate_variation.pl -downdb -buildver mm10 -webfrom ucsc snp142Common GRmm38db
+### download hg38 snp database Version snp151;  2 type snp,  total snp / Common snp  ~  10 / 1,     Common recommend
 
-### download mm10 region phastConsElements60way 
-perl annotate_variation.pl -build mm10 -downdb phastConsElements60way GRmm38db/ 
+perl annotate_variation.pl -downdb -buildver hg38 -webfrom ucsc snp151Common GRmm38db
+
+### download hg38 region phastConsElements60way 
+perl annotate_variation.pl -build hg38 -downdb phastConsElements100way GRmm38db/ 
 #####################################################################################################################
 1. sites  filter snp
 cd ~/scratch/20201122_A_I/HISAT2/process/mutation/ANNOVAR
